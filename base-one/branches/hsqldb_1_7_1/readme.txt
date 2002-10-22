443582530f1c924c5546984bf553fb8a3e3547e0
nobody <nobody@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2002-10-22 21:24:56 +0000

    This commit was manufactured by cvs2svn to create branch 'hsqldb_1_7_1'.

---
+++ base-one/branches/hsqldb_1_7_1/readme.txt
@@ -0,0 +1,61 @@
+Readme File
+
+2002.10.04
+
+HSQLDB 1.7.1
+
+HSQLDB 1.7.1 is a maintenance release, based on 1.7.0. A large
+number of bug fixes and minor feature enhancemnts, together with major
+speed and memory optimisations are included. No new keyword or major new
+capability has been added to 1.7.0.
+
+1.7.1 features major memory usage optimisations, resulting in up to 30%
+savings with memory tables and less with cached tables.
+
+This improvement is due to a rewrite on the internal Row and Index Node
+classes. While maintaining exactly the same orgininal logic, the new
+versions have a smaller memory footprint. This has also resulted in
+a modest improvement in speed.
+
+This release also features major speed improvements to all procedure
+calls and particularly the identity() function.
+
+The Transfer tool features new functionality, such as database
+dump and restore. Database Manager has been enhanced.
+
+Big improvements were made to startup speed where certain types of
+VIEW were defined. Also VIEWS featuring function calls are now
+supported.
+
+Almost all the bugs that were reported since the release of
+1.7.0 have been fixed. Some of these bugs related to data consistency
+after some ALTER TABLE commands were issued.
+
+A JAR file compiled with 1.3.0 is included in the /lib directory.
+
+The source has been tested with JDK 1.1.8, 1.3.0 and 1.4.0.
+Both the Ant (build.xml) and batch build methods are supported. The
+build scripts are all in the /build directory of the distribution.
+The new Ant script detects the JDK version that has been set up
+with the JAVA_HOME environment variable and compiles HSQLDB to run
+with that version of JVM.
+
+Also note that the enclosed JAR is the output of "ant jar" which
+excludes the test classes. You can now use "ant jartest" to build a
+more inclusive JAR which contains the test classes as well. You
+need the JUnit JAR in your classpath or in the /lib
+directory in order to compile the hsqldbtest.jar. This JAR is
+available from the JUnit SourceForge site.
+
+Some documentation is included in the /doc directory.
+changelog.txt lists the more important changes from version
+1.6.1. Various HTML files cover the new features of the software
+in more detail.
+
+Javadoc documentation features new, extensive coverage for all 
+jdbcXXXX.java files. This documentation covers the interaction between
+application programs and HSQLDB. Javadoc for public classes is
+included.
+
+Fred Toussi (fredt@users)
+http://hsqldb.sourceforge.net
---
+++ base-one/branches/hsqldb_1_7_1/src/org/hsqldb/CachedRow.java
@@ -0,0 +1,345 @@
+/* Copyrights and Licenses
+ *
+ * This product includes Hypersonic SQL.
+ * Originally developed by Thomas Mueller and the Hypersonic SQL Group. 
+ *
+ * Copyright (c) 1995-2000 by the Hypersonic SQL Group. All rights reserved. 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met: 
+ *     -  Redistributions of source code must retain the above copyright notice, this list of conditions
+ *         and the following disclaimer. 
+ *     -  Redistributions in binary form must reproduce the above copyright notice, this list of
+ *         conditions and the following disclaimer in the documentation and/or other materials
+ *         provided with the distribution. 
+ *     -  All advertising materials mentioning features or use of this software must display the
+ *        following acknowledgment: "This product includes Hypersonic SQL." 
+ *     -  Products derived from this software may not be called "Hypersonic SQL" nor may
+ *        "Hypersonic SQL" appear in their names without prior written permission of the
+ *         Hypersonic SQL Group. 
+ *     -  Redistributions of any form whatsoever must retain the following acknowledgment: "This
+ *          product includes Hypersonic SQL." 
+ * This software is provided "as is" and any expressed or implied warranties, including, but
+ * not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the Hypersonic SQL Group or its contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential damages (including, but
+ * not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+ * or business interruption). However caused any on any theory of liability, whether in contract,
+ * strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this
+ * software, even if advised of the possibility of such damage. 
+ * This software consists of voluntary contributions made by many individuals on behalf of the
+ * Hypersonic SQL Group.
+ *
+ *
+ * For work added by the HSQL Development Group:
+ *
+ * Copyright (c) 2001-2002, The HSQL Development Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer, including earlier
+ * license statements (above) and comply with all above license conditions.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution, including earlier
+ * license statements (above) and comply with all above license conditions.
+ *
+ * Neither the name of the HSQL Development Group nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL HSQL DEVELOPMENT GROUP, HSQLDB.ORG, 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package org.hsqldb;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+// fredt@users 20020221 - patch 513005 by sqlbob@users (RMP)
+// fredt@users 20020920 - path 1.7.1 by fredt - refactoring to cut mamory footprint
+
+/**
+ *  Disk and In-memory representation of a database row object with storage
+ *  independent methods for serialization and de-serialization.
+ *
+ * @version 1.7.1
+ */
+class CachedRow extends Row {
+
+    static final int NO_POS         = -1;
+    static int       iCurrentAccess = 0;
+    protected Table  tTable;
+    int              iLastAccess;
+    CachedRow        rLast, rNext;
+    int              iPos = NO_POS;
+    int              storageSize;
+    private boolean  bChanged;
+
+    /**
+     *  Constructor declaration
+     *
+     * @param  t
+     * @param  o
+     * @exception  SQLException  Description of the Exception
+     */
+    CachedRow(Table t, Object o[]) throws SQLException {
+
+        tTable = t;
+
+        int index = t.getIndexCount();
+
+        nPrimaryNode = Node.newNode(this, 0, t);
+
+        Node n = nPrimaryNode;
+
+        for (int i = 1; i < index; i++) {
+            n.nNext = Node.newNode(this, i, t);
+            n       = n.nNext;
+        }
+
+        oData       = o;
+        bChanged    = true;
+        iLastAccess = iCurrentAccess++;
+
+        t.putRow(this);
+    }
+
+    void setPos(int pos) {
+
+        iPos = pos;
+
+        Node n = nPrimaryNode;
+
+        while (n != null) {
+            n.setKey(pos);
+
+            n = n.nNext;
+        }
+    }
+
+    void changed() {
+        bChanged    = true;
+        iLastAccess = iCurrentAccess++;
+    }
+
+    boolean hasChanged() {
+        return (bChanged);
+    }
+
+    /**
+     * Get the node for a given index.
+     *
+     * @param  index
+     * @return the node
+     */
+    Node getNode(int index) {
+
+        iLastAccess = iCurrentAccess++;
+
+        return super.getNode(index);
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @param  n
+     * @return
+     */
+    Node getNextNode(Node n) {
+
+        iLastAccess = iCurrentAccess++;
+
+        return super.getNextNode(n);
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @return
+     */
+    Object[] getData() {
+
+        iLastAccess = iCurrentAccess++;
+
+        return oData;
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @return
+     */
+    Table getTable() {
+        return tTable;
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @param  before
+     */
+    void insert(CachedRow before) {
+
+        Record.memoryRecords++;
+
+        if (before == null) {
+            rNext = this;
+            rLast = this;
+        } else {
+            rNext        = before;
+            rLast        = before.rLast;
+            before.rLast = this;
+            rLast.rNext  = this;
+        }
+    }
+
+// fredt@users 20020221 - patch 513005 by sqlbob@users (RMP)
+// method renamed
+
+    /**
+     * Used only in Cache.java to avoid removing the row from the cache
+     *
+     *
+     * @return
+     * @throws  SQLException
+     */
+    boolean isRoot() throws SQLException {
+
+        Node n = nPrimaryNode;
+
+        while (n != null) {
+            if (Trace.DOASSERT) {
+                Trace.doAssert(n.getBalance() != -2);
+            }
+
+            if (Trace.STOP) {
+                Trace.stop();
+            }
+
+            if (n.isRoot()) {
+                return true;
+            }
+
+            n = n.nNext;
+        }
+
+        return false;
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @param  out            Description of the Parameter
+     * @throws  IOException
+     * @throws  SQLException
+     */
+    void write(DatabaseRowOutputInterface out)
+    throws IOException, SQLException {
+
+        out.writeSize(storageSize);
+
+        if (tTable.isIndexCached()) {
+            Node n = nPrimaryNode;
+
+            while (n != null) {
+                n.write(out);
+
+                n = n.nNext;
+            }
+        }
+
+        out.writeData(oData, tTable);
+        out.writePos(iPos);
+
+        bChanged = false;
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @throws  SQLException
+     */
+    void delete() throws SQLException {
+
+        Record.memoryRecords++;
+
+        bChanged = false;
+
+        tTable.removeRow(this);
+
+        rNext        = null;
+        rLast        = null;
+        tTable       = null;
+        oData        = null;
+        nPrimaryNode = null;
+    }
+
+    /**
+     *  Method declaration
+     *
+     * @throws  SQLException
+     */
+    void free() throws SQLException {
+
+        rLast.rNext = rNext;
+        rNext.rLast = rLast;
+
+        if (rNext == this) {
+            rNext = rLast = null;
+        }
+    }
+
+    /**
+     *  constructor when read from cache
+     *
+     * @param  t
+     * @param  in
+     * @exception  IOException   Description of the Exception
+     * @exception  SQLException  Description of the Exception
+     */
+    CachedRow(Table t,
+              DatabaseRowInputInterface in) throws IOException, SQLException {
+
+        tTable      = t;
+        iPos        = in.getPos();
+        storageSize = in.getSize();
+
+        int index = t.getIndexCount();
+
+        nPrimaryNode = Node.newNode(this, in, 0, t);
+
+        Node n = nPrimaryNode;
+
+        if (t.getIndexType() == Index.POINTER_INDEX) {
+            ((PointerNode) nPrimaryNode).setNextKey(in.getNextPos());
+        }
+
+        for (int i = 1; i < index; i++) {
+            n.nNext = Node.newNode(this, in, i, t);
+            n       = n.nNext;
+        }
+
+        oData = in.readData(tTable.getColumnTypes());
+
+        if (tTable.isIndexCached()) {
+            Trace.check(in.readIntData() == iPos, Trace.INPUTSTREAM_ERROR);
+        }
+
+        iLastAccess = iCurrentAccess++;
+    }
+}
