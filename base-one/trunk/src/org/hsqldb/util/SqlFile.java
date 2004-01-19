2a0ff0bb03ab3e1bbc89f4388b623928942d4a78
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-01-19 19:50:15 +0000

    Just a skeleton

---
+++ base-one/trunk/src/org/hsqldb/util/SqlFile.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, The HSQL Development Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the HSQL Development Group nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL HSQL DEVELOPMENT GROUP, HSQLDB.ORG, 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hsqldb.util;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+import java.sql.Connection;
+
+public class SqlFile {
+    private File file;
+    private boolean contOnErr;
+
+    SqlFile(File inFile, boolean inContOnErr) throws IOException {
+        file = inFile;
+        contOnErr = inContOnErr;
+        if (file != null && !file.canRead()) {
+            throw new IOException("Can't read SQL file '" + file + "'");
+        }
+    }
+
+    SqlFile(boolean inContOnErr) throws IOException {
+        this(null, inContOnErr);
+    }
+
+    public void execute(Connection conn) {
+        execute(conn, System.out, System.err);
+    }
+
+    public void execute(Connection conn, PrintStream psStd,
+            PrintStream psErr) {
+        psErr.println("Executing '" + file + "'");
+    }
+}
