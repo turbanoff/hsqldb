599d71f64538d21edf256e41e064da1f17f7e3a7
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-01-19 19:50:52 +0000

    In good shape.  Now uses the new SqlFile class.

--- base-one/trunk/src/org/hsqldb/util/SqlTool.java
+++ base-one/trunk/src/org/hsqldb/util/SqlTool.java
@@ -1,4 +1,7 @@
-/* Copyright (c) 2001-2003, The HSQL Development Group
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,39 +31,27 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 package org.hsqldb.util;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.applet.*;
-import java.sql.*;
-import java.net.*;
-import java.io.*;
-import java.util.*;
-import org.hsqldb.lib.java.javaSystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
 
 /**
  * Sql Tool.  A command-line and/or interactive SQL tool.
- *
- * @version $Revision: 1.1 $
  * (Note:  For every Javadoc block comment, I'm using a single blank line 
  *  immediately after the description, just like's Sun's examples in
  *  their Coding Conventions document).
+ *
+ * @version $Revision: 1.2 $
  */
 public class SqlTool {
-    /*
-     * This is starting off from a copy of ScriptTool.java.
-     * I intend to use existing Hsqldb objects as much as possible.
-     */
-
-    private static Properties pProperties = new Properties();
-    private Connection        cConn;
-    private Statement         sStatement;
-    //private boolean           BATCH = true;
-    private boolean           BATCH = false;
-    private String            EKW   = new String("go");
-    private boolean           EOF   = false;
+    final static private String DEFAULT_JDBC_DRIVER = "org.hsqldb.jdbcDriver";
+    static private Connection conn;
 
     final static private String DEFAULT_RCFILE =
             System.getProperty("user.home") + "/hsqldb.rc";
@@ -70,6 +61,8 @@ public class SqlTool {
      * If it is anticipated that SqlTool.execute() will be executed
      * from other code directly rather than from SqlTool.main(), then
      * make this a global-level class.
+     * I expect other Java code to invoke SqlFile.execute(), but not
+     * anything in this class.
      */
     private static class ConnectData {
         public void report() {
@@ -105,6 +98,10 @@ public class SqlTool {
                 }
                 keyword = tokenizer.nextToken();
                 value = tokenizer.nextToken();
+                if (dbKey == null) {
+                    if (keyword.equals("urlid")) System.out.println(value);
+                    continue;
+                }
                 if (keyword.equals("urlid")) {
                     if (value.equals(dbKey)) {
                         if (id == null) {
@@ -133,6 +130,7 @@ public class SqlTool {
                     }
                 }
             }
+            if (dbKey == null) return;
             if (url == null || username == null || password == null) {
                 throw new Exception("url or username or password not set "
                         + "for '" + dbKey + "' in file '" + file + "'");
@@ -148,8 +146,11 @@ public class SqlTool {
             "Usage: java SqlTool [--optname [optval...]] urlid [file1.sql...]\n"
             + "where arguments are:\n"
             + "    --help                   Prints this message\n"
-            + "    --rcfile /file/path.rc   Connect Info File\n"
-            + "    --driver a.b.c.Driver    Non-hsqldb JDBC driver class\n"
+            + "    --list                   List urlids in the rcfile\n"
+            + "    --debug                  print Debug info to stderr\n"
+        + "    --rcfile /file/path.rc   Connect Info File [$HOME/hsqldb.rc]\n"
+            + "    --driver a.b.c.Driver    JDBC driver class ["
+            +           DEFAULT_JDBC_DRIVER + "]\n"
         + "    urlid                    ID of url/userame/password in rcfile\n"
             + "    file1.sql...             SQL files to be executed";
 
@@ -164,10 +165,12 @@ public class SqlTool {
      */
     public static void main(String arg[]) {
         String rcFile = null;
-        String driver = null;
+        String driver = DEFAULT_JDBC_DRIVER;
         String targetDb = null;
-        File[] scriptFiles = null;
+        boolean debug = false;
+        File[] scriptFiles = { null };
         int i = -1;
+        boolean listMode = false;
 
         try {
             while ((i + 1 < arg.length) && arg[i+1].startsWith("--")) {
@@ -177,9 +180,17 @@ public class SqlTool {
                     System.out.println(SYNTAX_MESSAGE);
                     System.exit(0);
                 }
+                if (arg[i].substring(2).equals("list")) {
+                    listMode = true;
+                    continue;
+                }
                 if (arg[i].substring(2).equals("rcfile")) {
                     if (++i == arg.length) throw bcl;
                     rcFile = arg[i];
+                    continue;
+                }
+                if (arg[i].substring(2).equals("debug")) {
+                    debug = true;
                     continue;
                 }
                 if (arg[i].substring(2).equals("driver")) {
@@ -189,12 +200,18 @@ public class SqlTool {
                 }
                 throw bcl;
             }
-            if (++i == arg.length) throw bcl;
-            targetDb = arg[i];
+            if (!listMode) {
+                if (++i == arg.length) throw bcl;
+                targetDb = arg[i];
+            }
             int scriptIndex = 0;
             if (arg.length > i + 1) {
                 scriptFiles = new File[arg.length - i - 1];
-System.err.println("scriptFiles has " + scriptFiles.length + " elements");
+                if (debug) {
+                    System.err.println(
+                            "scriptFiles has " + scriptFiles.length 
+                                    + " elements");
+                }
                 while (i + 1 < arg.length) {
                     scriptFiles[scriptIndex++]  = new File(arg[++i]);
                 }
@@ -202,15 +219,6 @@ System.err.println("scriptFiles has " + scriptFiles.length + " elements");
         } catch (BadCmdline bcl) {
             System.err.println(SYNTAX_MESSAGE);
             System.exit(2);
-        }
-        SerialFileReader sfr = null;
-        try {
-            sfr = new SerialFileReader(scriptFiles);
-        } catch (Exception e) {
-            System.err.println(
-                    "Failed to obtain SQL script file(s).  " + e + ": "
-                     + e.getMessage());
-            System.exit(1);
         }
         ConnectData conData = null;
         try {
@@ -221,191 +229,40 @@ System.err.println("scriptFiles has " + scriptFiles.length + " elements");
                     + targetDb + "': " + e.getMessage());
             System.exit(1);
         }
-        // conData.report();
-
-        (new SqlTool()).execute(conData, sfr, driver);
-        System.exit(0);
-    }    // end main
-
-    /**
-     * Execute the given sql files.
-     * To run this without SqlTool.main(), change the scope of
-     * ConnectData as needed.
-     * It's thread-safe but single-threaded (synchronized).  See the
-     * class Javadocs for justification.
-     */
-    public synchronized void execute(ConnectData cData,
-            SerialFileReader sfReader, String inDriver) {
-        EOF = false;
-
-        Properties     p        = pProperties;
-        String driver = ((inDriver == null)
-                         ? "org.hsqldb.jdbcDriver"
-                         : inDriver);
-        boolean log = p.getProperty("log", "false").equalsIgnoreCase("true");
-
-        //BATCH = p.getProperty("batch", "true").equalsIgnoreCase("true");
-
-        if (log) {
-            trace("driver   = " + driver);
-            trace("url      = " + cData.url);
-            trace("username = " + cData.username);
-            trace("password = " + cData.password);
-            //trace("scripts  = " + script);
-            trace("log      = " + log);
-            trace("batch    = " + BATCH);
-            javaSystem.setLogToSystem(true);
-        }
+        if (listMode) System.exit(0);
+        if (debug) conData.report();
 
         try {
             // As described in the JDBC FAQ:
             // http://java.sun.com/products/jdbc/jdbc-frequent.html;
             // Why doesn't calling class.forName() load my JDBC driver?
-            // There is a bug in the JDK 1.1.x that can cause Class.forName() to fail.
-            // new org.hsqldb.jdbcDriver();
+            // There is a bug in the JDK 1.1.x that can cause Class.forName()
+            // to fail. // new org.hsqldb.jdbcDriver();
             Class.forName(driver).newInstance();
 
-            cConn = DriverManager.getConnection(cData.url, cData.username,
-                                                cData.password);
+            conn = DriverManager.getConnection(conData.url, conData.username,
+                                                conData.password);
         } catch (Exception e) {
             System.err.println(
-                    "Failed to get a connection to " + cData.url
+                    "Failed to get a connection to " + conData.url
                     + ".  " + e.getMessage());
             //e.printStackTrace();
-            // How about not continuing as if nothing is wrong?
+            // Let's not continuing as if nothing is wrong.
             throw new RuntimeException(e);
         }
 
+        SqlFile[] sqlFiles = new SqlFile[scriptFiles.length];
         try {
-            sStatement = cConn.createStatement();
-
-            String sql;
-
-            while ((sql = fileToString(sfReader)) != null) {
-                if (sql.length() == 1) {
-                    continue;
-                }
-
-                if (log) {
-                    trace("SQL (" + sfReader.getCurFileName() + ':'
-                            + sfReader.getCurLine() + ") : "
-                          + sql.substring(0, sql.length() - 2));
-                }
-
-                sStatement.execute(sql);
-
-                ResultSet results     = sStatement.getResultSet();
-                int       updateCount = sStatement.getUpdateCount();
-
-                if (updateCount == -1) {
-                    trace(toString(results));
-                } else {
-                    trace("update count " + updateCount);
-                }
-            }
-        } catch (SQLException e) {
-            System.err.println("SQL Error at "
-                    + sfReader.getCurFileName() + ':'
-                    + sfReader.getCurLine() + ": " + e);
+            for (int j = 0; j < scriptFiles.length; j++)
+                sqlFiles[j] =
+                        new SqlFile(scriptFiles[j], (scriptFiles == null));
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+            System.exit(2);
         }
 
-        try {
-            cConn.close();
-            sfReader.close();
-        } catch (Exception ce) {}
-    }
-
-    /**
-     * Translate ResultSet to String representation
-     * @param r
-     */
-    private String toString(ResultSet r) {
-
-        try {
-            if (r == null) {
-                return "No Result";
-            }
-
-            ResultSetMetaData m      = r.getMetaData();
-            int               col    = m.getColumnCount();
-            StringBuffer      strbuf = new StringBuffer();
-
-            for (int i = 1; i <= col; i++) {
-                strbuf = strbuf.append(m.getColumnLabel(i) + "\t");
-            }
-
-            strbuf = strbuf.append("\n");
-
-            while (r.next()) {
-                for (int i = 1; i <= col; i++) {
-                    strbuf = strbuf.append(r.getString(i) + "\t");
-
-                    if (r.wasNull()) {
-                        strbuf = strbuf.append("(null)\t");
-                    }
-                }
-
-                strbuf = strbuf.append("\n");
-            }
-
-            return strbuf.toString();
-        } catch (SQLException e) {
-            return null;
-        }
-    }
-
-    /**
-     * Read file and convert it to string.
-     */
-    private String fileToString(BufferedReader in) {
-
-        if (EOF) {
-            return null;
-        }
-
-        EOF = true;
-
-        StringBuffer a = new StringBuffer();
-
-        try {
-            String line;
-
-            while ((line = in.readLine()) != null) {
-                if (BATCH) {
-                    if (line.startsWith("print ")) {
-                        trace("\n" + line.substring(5));
-
-                        continue;
-                    }
-
-                    if (line.equalsIgnoreCase(EKW)) {
-                        EOF = false;
-
-                        break;
-                    }
-                }
-
-                a.append(line);
-                a.append('\n');
-            }
-
-            a.append('\n');
-
-            return a.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    /**
-     * Method declaration
-     *
-     *
-     * @param s
-     */
-    private void trace(String s) {
-        System.out.println(s);
+        for (int j = 0; j < scriptFiles.length; j++)
+            sqlFiles[j].execute(conn);
+        System.exit(0);
     }
 }
