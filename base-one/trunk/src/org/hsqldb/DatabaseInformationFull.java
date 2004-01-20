bc119e1435126fa102e9a56a7899c17ae5dfcadf
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-01-20 22:03:20 +0000

    Removed binary chars that break some javac compilers

--- base-one/trunk/src/org/hsqldb/DatabaseInformationFull.java
+++ base-one/trunk/src/org/hsqldb/DatabaseInformationFull.java
@@ -2365,7 +2365,7 @@ extends org.hsqldb.DatabaseInformationMain {
      *      value of GRANTEE forthe usage privilege being described. <p>
      *
      * <li> The value of GRANTEE is the &lt;authorization identifier&gt; of some
-     *      user or role, or “PUBLIC” to indicate all users, to whom the usage
+     *      user or role, or  PUBLIC  to indicate all users, to whom the usage
      *      privilege being described is granted. <p>
      *
      * <li> The values of OBJECT_CATALOG, OBJECT_SCHEMA, and OBJECT_NAME are the
