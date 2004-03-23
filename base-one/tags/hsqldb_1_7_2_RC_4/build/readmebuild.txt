afd8da70e1c5e3e25a73ad99dbc3dbbee8b790e5
nobody <nobody@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-03-23 11:56:21 +0000

    This commit was manufactured by cvs2svn to create tag 'hsqldb_1_7_2_RC_4'.

---
+++ base-one/tags/hsqldb_1_7_2_RC_4/build/readmebuild.txt
@@ -0,0 +1,116 @@
+Build instructions for HSQLDB 1.7.2
+
+
+A jar file for HSQLDB is provided in the /lib directory of the 
+.zip package. This jar contains both the database and the 
+utilities and has been built with JDK 1.3.1. (#RELEASE VERSION)
+
+The supplied jar can be used with JRE version 1.1.x., 1.2.x,
+1.3.x and 1.4.x. No recompilation is necessary.
+
+Possible reasons to rebuild the jar are:
+
+(a) Rebuild with JDK 1.4 to gain cccess to JDBC 3 methods and 
+classes such as Savepoint and ParameterMetaData. Also to
+speed up large databases with CACHED tables via the nio classes.
+An HSQLDB jar built with JDK 1.4 cannot be used with older JRE
+versions.
+
+(b) Rebuild with any JDK to reduce the size of the jar for small
+devices or for better download speed.
+
+(c) Rebuild to work around known compatibility issues with
+certain non-standard JDK's.
+
+
+The source files
+
+The source files are supplied in a state that is compatible with
+JDK 1.3. Features that are specific to JDK 1.4 are enclosed in 
+comments containing preprocessor directives.
+
+The org.hsqldb.util.CodeSwitcher class can process
+the source files and make them compatible with a given JDK by
+removing or adding comments from blocks of code. This procedure
+is invoked with the supplied build scripts.
+
+
+Different jar sizes
+
+HSQLDB can be built in any combination of four different sizes.
+
+The smallest jar size (hsqldbmin.jar) contains only the database
+and JDBC support for in-process mode databases. The next smallest
+jar size (hsqldbmain.jar) also contains support for server modes.
+The default size (hsqldb.jar) additionally contains the
+utilities such as Database Manager and Transfer Tool. The largest 
+size (hsqldbtest.jar) includes some test classes as well. You need
+the JUnit jar in the /lib directory in order to build and run the
+test classes.
+
+
+
+JDK and JRE versions
+
+You can use any recent JDK for building the jar. Use of JDK 1.3.x is
+recommended for the widest compatibility.
+
+Javadoc can be built with Ant and batch files.
+
+The JDK used for the build has a marginal effect on the size.
+Newer JDK's support more JDBC methods and classes, resulting in
+slightly larger jars. JDK 1.1 is not as advanced as the newer ones
+and produces larger class files.
+
+JDK 1.1.x
+
+It is recommended not to use JDK 1.1.x for building the
+jar, as this version produces much larger jar sizes and the result
+is not upward compatible with newer JDK'S / JRE's. Use JDK 1.3.x
+to build the jar instead. You can then deploy the jar in JRE 1.1.
+
+
+
+Build methods:
+
+The preferred method of rebuilding the jar is with Ant. After
+installing Ant on your system use the following command from the
+/build directory:
+
+ant
+
+The command displays a list of different options for building 
+different sizes of the HSQLDB Jar. The default jar is built using:
+
+ant jar
+
+The Ant method always builds a jar with the JDK that is used by Ant
+and specified in the JAVA_HOME environment variable. The script
+automatically converts the source files for compatibility with the
+given JDK.
+
+Before building the hsqldbtest.jar package, you should download the
+junit.jar and put it in the /lib directory, alongside servlet.jar, 
+which is included in the .zip package.
+
+Batch Build
+
+The Unix / Linux builds have been written by Blaine Simpson and
+support a wide range of JDK's.
+
+A set of MSDOS batch files is also provided. These produce only
+the default jar size. The path and classpath variables for the JDK
+should of course be set before running any of the batch files.
+
+If you are compiling for JDK's other than 1.2.x or 1.3.x, you should
+use the appropriate switchtoJDK11.bat or switchtoJDK14.bat to adapt
+the source files to the target JDK before running the appropriate
+buildJDK11.bat or buildJDK14.bat
+
+Linux build
+
+Linux scripts with the same functionality as the MSDOS batch files
+are provided.
+
+
+fredt@users
---
+++ base-one/tags/hsqldb_1_7_2_RC_4/src/org/hsqldb/Like.java
@@ -0,0 +1,358 @@
+/* Copyrights and Licenses
+ *
+ * This product includes Hypersonic SQL.
+ * Originally developed by Thomas Mueller and the Hypersonic SQL Group. 
+ *
+ * Copyright (c) 1995-2000 by the Hypersonic SQL Group. All rights reserved. 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met: 
+ *     -  Redistributions of source code must retain the above copyright notice, this list of conditions
+ *         and the following disclaimer. 
+ *     -  Redistributions in binary form must reproduce the above copyright notice, this list of
+ *         conditions and the following disclaimer in the documentation and/or other materials
+ *         provided with the distribution. 
+ *     -  All advertising materials mentioning features or use of this software must display the
+ *        following acknowledgment: "This product includes Hypersonic SQL." 
+ *     -  Products derived from this software may not be called "Hypersonic SQL" nor may
+ *        "Hypersonic SQL" appear in their names without prior written permission of the
+ *         Hypersonic SQL Group. 
+ *     -  Redistributions of any form whatsoever must retain the following acknowledgment: "This
+ *          product includes Hypersonic SQL." 
+ * This software is provided "as is" and any expressed or implied warranties, including, but
+ * not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the Hypersonic SQL Group or its contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential damages (including, but
+ * not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+ * or business interruption). However caused any on any theory of liability, whether in contract,
+ * strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this
+ * software, even if advised of the possibility of such damage. 
+ * This software consists of voluntary contributions made by many individuals on behalf of the
+ * Hypersonic SQL Group.
+ *
+ *
+ * For work added by the HSQL Development Group:
+ *
+ * Copyright (c) 2001-2004, The HSQL Development Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer, including earlier
+ * license statements (above) and comply with all above license conditions.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution, including earlier
+ * license statements (above) and comply with all above license conditions.
+ *
+ * Neither the name of the HSQL Development Group nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL HSQL DEVELOPMENT GROUP, HSQLDB.ORG, 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package org.hsqldb;
+
+/**
+ * Reusable object for processing LIKE queries.
+ *
+ * @version 1.7.2
+ */
+
+// boucherb@users 20030930 - patch 1.7.2 - optimize into joins if possible
+// fredt@users 20031006 - patch 1.7.2 - reuse Like objects for all rows
+class Like {
+
+    private char[]   cLike;
+    private int[]    wildCardType;
+    private int      iLen;
+    private boolean  isIgnoreCase;
+    private int      iFirstWildCard;
+    private boolean  isNull;
+    Character        escapeChar;
+    boolean          optimised;
+    static final int UNDERSCORE_CHAR = 1;
+    static final int PERCENT_CHAR    = 2;
+
+    Like(Character escape) {
+        escapeChar = escape;
+    }
+
+    /**
+     * param setter
+     *
+     * @param s
+     * @param escape
+     * @param ignorecase
+     */
+    void setParams(String s, boolean ignorecase) {
+
+        isIgnoreCase = ignorecase;
+
+        normalize(s, true);
+
+        optimised = true;
+    }
+
+    /**
+     * Resets the search pattern;
+     */
+    void resetPattern(String s) {
+        normalize(s, true);
+    }
+
+    private String getStartsWith() {
+
+        StringBuffer s = new StringBuffer();
+        int          i = 0;
+
+        for (; (i < iLen) && (wildCardType[i] == 0); i++) {
+            s.append(cLike[i]);
+        }
+
+        if (i == 0) {
+            return null;
+        }
+
+        return s.toString();
+    }
+
+    /**
+     * Method declaration
+     *
+     *
+     * @param o
+     *
+     * @return
+     */
+    boolean compare(Object o) {
+
+        if (o == null) {
+            return iLen == 0;
+        }
+
+        String s = o.toString();
+
+        if (isIgnoreCase) {
+            s = s.toUpperCase();
+        }
+
+        return compareAt(s, 0, 0, s.length());
+    }
+
+    /**
+     * Method declaration
+     *
+     *
+     * @param s
+     * @param i
+     * @param j
+     * @param jLen
+     *
+     * @return
+     */
+    private boolean compareAt(String s, int i, int j, int jLen) {
+
+        for (; i < iLen; i++) {
+            switch (wildCardType[i]) {
+
+                case 0 :                  // general character
+                    if ((j >= jLen) || (cLike[i] != s.charAt(j++))) {
+                        return false;
+                    }
+                    break;
+
+                case UNDERSCORE_CHAR :    // underscore: do not test this character
+                    if (j++ >= jLen) {
+                        return false;
+                    }
+                    break;
+
+                case PERCENT_CHAR :       // percent: none or any character(s)
+                    if (++i >= iLen) {
+                        return true;
+                    }
+
+                    while (j < jLen) {
+                        if ((cLike[i] == s.charAt(j))
+                                && compareAt(s, i, j, jLen)) {
+                            return true;
+                        }
+
+                        j++;
+                    }
+
+                    return false;
+            }
+        }
+
+        if (j != jLen) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Method declaration
+     *
+     *
+     * @param s
+     * @param b
+     * @param e
+     */
+    private void normalize(String pattern, boolean b) {
+
+        isNull = pattern == null;
+
+        if (!isNull && isIgnoreCase) {
+            pattern = pattern.toUpperCase();
+        }
+
+        iLen           = 0;
+        iFirstWildCard = -1;
+
+        int l = pattern == null ? 0
+                                : pattern.length();
+
+        cLike        = new char[l];
+        wildCardType = new int[l];
+
+        boolean bEscaping = false,
+                bPercent  = false;
+
+        for (int i = 0; i < l; i++) {
+            char c = pattern.charAt(i);
+
+            if (bEscaping == false) {
+                if (b && (escapeChar != null
+                          && escapeChar.charValue() == c)) {
+                    bEscaping = true;
+
+                    continue;
+                } else if (c == '_') {
+                    wildCardType[iLen] = UNDERSCORE_CHAR;
+
+                    if (iFirstWildCard == -1) {
+                        iFirstWildCard = iLen;
+                    }
+                } else if (c == '%') {
+                    if (bPercent) {
+                        continue;
+                    }
+
+                    bPercent           = true;
+                    wildCardType[iLen] = PERCENT_CHAR;
+
+                    if (iFirstWildCard == -1) {
+                        iFirstWildCard = iLen;
+                    }
+                } else {
+                    bPercent = false;
+                }
+            } else {
+                bPercent  = false;
+                bEscaping = false;
+            }
+
+            cLike[iLen++] = c;
+        }
+
+        for (int i = 0; i < iLen - 1; i++) {
+            if ((wildCardType[i] == PERCENT_CHAR)
+                    && (wildCardType[i + 1] == UNDERSCORE_CHAR)) {
+                wildCardType[i]     = UNDERSCORE_CHAR;
+                wildCardType[i + 1] = PERCENT_CHAR;
+            }
+        }
+    }
+
+    boolean hasWildcards() {
+        return iFirstWildCard != -1;
+    }
+
+    boolean isEquivalentToFalsePredicate() {
+        return isNull;
+    }
+
+    boolean isEquivalentToEqualsPredicate() {
+        return iFirstWildCard == -1;
+    }
+
+    boolean isEquivalentToNotNullPredicate() {
+
+        if (isNull ||!hasWildcards()) {
+            return false;
+        }
+
+        for (int i = 0; i < wildCardType.length; i++) {
+            if (wildCardType[i] != PERCENT_CHAR) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean isEquivalentToBetweenPredicate() {
+
+        return iFirstWildCard > 0
+               && iFirstWildCard == wildCardType.length - 1
+               && cLike[iFirstWildCard] == '%';
+    }
+
+    boolean isEquivalentToBetweenPredicateAugmentedWithLike() {
+        return iFirstWildCard > 0 && cLike[iFirstWildCard] == '%';
+    }
+
+    String getRangeLow() {
+        return getStartsWith();
+    }
+
+    String getRangeHigh() {
+
+        String s = getStartsWith();
+
+        return s == null ? null
+                         : s.concat("\uffff");
+    }
+
+    public String toString() {
+
+        return super.toString() + "[\n" + "bIgnoreCase=" + isIgnoreCase
+               + '\n' + "iLen=" + iLen + '\n' + "cLike="
+               + org.hsqldb.lib.StringUtil.arrayToString(cLike) + '\n'
+               + "iType="
+               + org.hsqldb.lib.StringUtil.arrayToString(wildCardType) + ']';
+    }
+/*
+    static int indexOf(String s, int start, char search, String escape) {
+
+        // PRE:
+        // s != null
+        // s.length() > 0
+        // start >= 0 and start < s.length()
+        int pos = s.indexOf(search, start);
+
+        while (pos > -1 && s.regionMatches(pos - 1, escape, 0, 1)) {
+            pos = s.indexOf(search, pos + 1);
+        }
+
+        return pos;
+    }
+*/
+}
