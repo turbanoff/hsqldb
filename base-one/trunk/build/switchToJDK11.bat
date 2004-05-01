46315159de61763f571b75d49f42e868423b5874
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-05-01 23:09:58 +0000

    post RC5 updates

--- base-one/trunk/build/switchToJDK11.bat
+++ base-one/trunk/build/switchToJDK11.bat
@@ -16,6 +16,6 @@ java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org
 java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcConnection.java -JAVA2 -JDBC3
 java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcDatabaseMetaData.java -JAVA2 -JDBC3
 java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcPreparedStatement.java -JAVA2 -JDBC3
-java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcResultSet.java +JAVA2 -JDBC3
+java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcResultSet.java -JAVA2 -JDBC3
 java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/jdbc/jdbcStatement.java -JAVA2 -JDBC3
-java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/HsqlDateTime.java +JAVA2 -JDBC3
+java -classpath "%classpath%;../classes" org.hsqldb.util.CodeSwitcher ../src/org/hsqldb/HsqlDateTime.java -JAVA2 -JDBC3
