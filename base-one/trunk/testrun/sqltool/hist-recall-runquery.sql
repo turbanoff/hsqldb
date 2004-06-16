a7fcc464bfe9475696f95067c280660ec1244fbc
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2004-06-16 19:36:07 +0000

    works

---
+++ base-one/trunk/testrun/sqltool/hist-recall-runquery.sql
@@ -0,0 +1,23 @@
+/*
+    $Id: hist-recall-19.sql,v 1.1 2004/06/16 19:26:24 unsaved Exp $
+
+    See readme.txt in this directory for how to unit test SqlTool.
+
+    Tests:  Recall a SQL query from the SQL buffer and execute it.
+
+    HARNESS_METADATA        BEGIN         
+    requireStdoutRegex  (?mis)recalling and executing now:.*select \* from t\b.*^31$
+    arg mem 
+    HARNESS_METADATA        END       
+*/
+
+/* The blank line after a command moves the command to history without
+   executing it. */
+select * from t
+
+create table t (i int);
+insert into t values(31);
+
+
+\p Recalling and executing now:
+\-2;
