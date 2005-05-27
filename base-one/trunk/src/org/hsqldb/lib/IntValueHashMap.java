8e90248c36d10103bfbd34b24876bd3988d4761a
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2005-05-27 22:18:50 +0000

    implemented putAll()

--- base-one/trunk/src/org/hsqldb/lib/IntValueHashMap.java
+++ base-one/trunk/src/org/hsqldb/lib/IntValueHashMap.java
@@ -200,4 +200,16 @@ public class IntValueHashMap extends BaseHashMap {
             IntValueHashMap.this.clear();
         }
     }
+
+    public void putAll(IntValueHashMap t) {
+
+        Iterator it = t.keySet().iterator();
+
+        while (it.hasNext()) {
+            Object key = it.next();
+
+            put(key, t.get(key));
+        }
+    }
+
 }
