c8ccb3625ed2ea9569f58ba3c6001af8705e9871
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2005-05-27 15:37:32 +0000

    post 1_8_0 RC10

--- base-one/trunk/build/buildJDK12.bat
+++ base-one/trunk/build/buildJDK12.bat
@@ -9,19 +9,19 @@ mkdir ..\temp
 copy org\hsqldb\jdbc\jdbcDataSource*.java ..\temp\
 copy org\hsqldb\jdbc\jdbcSavepoint.java ..\temp\
 copy org\hsqldb\jdbc\jdbcParameterMetaData.java ..\temp\
-copy org\hsqldb\NIOLockFile.java ..\temp\
-copy org\hsqldb\NIOScaledRAFile.java ..\temp\
+copy org\hsqldb\persist\NIOLockFile.java ..\temp\
+copy org\hsqldb\persist\NIOScaledRAFile.java ..\temp\
 del org\hsqldb\jdbc\jdbcDataSource*.java
 del org\hsqldb\jdbc\jdbcSavepoint.java
 del org\hsqldb\jdbc\jdbcParameterMetaData.java
-del org\hsqldb\NIOLockFile.java
-del org\hsqldb\NIOScaledRAFile.java
-javac -O -nowarn -d ../classes -classpath "%classpath%;../classes;../lib/servlet.jar;." ./*.java org/hsqldb/*.java org/hsqldb/jdbc/*.java org/hsqldb/rowio/*.java org/hsqldb/scriptio/*.java org/hsqldb/store/*.java org/hsqldb/lib/*.java org/hsqldb/util/*.java
+del org\hsqldb\persist\NIOLockFile.java
+del org\hsqldb\persist\NIOScaledRAFile.java
+javac -O -nowarn -d ../classes -classpath "%classpath%;../classes;../lib/servlet.jar;." ./*.java org/hsqldb/*.java org/hsqldb/jdbc/*.java org/hsqldb/persist/*.java org/hsqldb/rowio/*.java org/hsqldb/scriptio/*.java org/hsqldb/store/*.java org/hsqldb/lib/*.java org/hsqldb/util/*.java
 copy ..\temp\jdbcDataSource*.java org\hsqldb\jdbc
 copy ..\temp\jdbcSavepoint.java org\hsqldb\jdbc
 copy ..\temp\jdbcParameterMetaData.java org\hsqldb\jdbc
-copy ..\temp\NIOLockFile.java org\hsqldb
-copy ..\temp\NIOScaledRAFile.java org\hsqldb
+copy ..\temp\NIOLockFile.java org\hsqldb\persist
+copy ..\temp\NIOScaledRAFile.java org\hsqldb\persist
 del ..\temp\jdbcDataSource*.java
 del ..\temp\jdbcSavepoint.java
 del ..\temp\jdbcParameterMetaData.java
@@ -29,7 +29,7 @@ del ..\temp\NIOLockFile.java
 del ..\temp\NIOScaledRAFile.java
 rmdir ..\temp
 cd ..\classes
-copy ..\src\org\hsqldb\util\hsqldb.gif org\hsqldb\util
-jar -cf ../lib/hsqldb.jar *.class org/hsqldb/*.class org/hsqldb/jdbc/*.class org/hsqldb/rowio/*.class org/hsqldb/scriptio/*.class org/hsqldb/store/*.class org/hsqldb/lib/*.class org/hsqldb/util/*.class  org/hsqldb/util/*.gif
+copy ..\src\org\hsqldb\util\*.gif org\hsqldb\util
+jar -cf ../lib/hsqldb.jar *.class org/hsqldb/*.class org/hsqldb/jdbc/*.class org/hsqldb/persist/*.class org/hsqldb/rowio/*.class org/hsqldb/scriptio/*.class org/hsqldb/store/*.class org/hsqldb/lib/*.class org/hsqldb/util/*.class  org/hsqldb/util/*.gif
 cd ..\build
 pause
--- base-one/trunk/readme.txt
+++ base-one/trunk/readme.txt
@@ -2,10 +2,7 @@ Readme File
 
 May 2005
 
-This package contains the RELEASE CANDIDATE 10 of HSQLDB 1.8.0
-
-DO NOT USE THIS SOFTWARE IN PRODUCTION ENVIRONMENTS. THIS IS RELEASED FOR PUBLIC
-TESTS ONLY. WAIT FOR A RELEASE VERSION FOR USE IN PRODUCTION ENVIRONMENTS. 
+This package contains HSQLDB 1.8.0
 
 HSQLDB is a relational database engine and a set of tools written in Java.
 
@@ -14,3 +11,5 @@ The file index.html in this directory contains the list of directories with thei
 Documentation and license information can be found in the /doc directory.
 
 Project home page: http://hsqldb.sourceforge.net
+
+Please check the site periodically for updated versions.
