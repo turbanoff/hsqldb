ac1e3b66386a5c88b0044931899892b24dd85843
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2005-07-19 23:17:07 +0000

    Fixed problem where oversized scripts were not being executed

--- base-one/trunk/src/org/hsqldb/util/DatabaseManagerSwing.java
+++ base-one/trunk/src/org/hsqldb/util/DatabaseManagerSwing.java
@@ -247,9 +247,9 @@ implements ActionListener, WindowListener, KeyListener {
         "See the forums, mailing lists, and HSQLDB User Guide\n"
         + "at http://hsqldb.org.\n\n"
         + "Please paste the following version identifier with any\n"
-        + "problem reports or help requests:  $Revision: 1.57 $";
+        + "problem reports or help requests:  $Revision: 1.58 $";
     private static final String ABOUT_TEXT =
-        "$Revision: 1.57 $ of DatabaseManagerSwing\n\n"
+        "$Revision: 1.58 $ of DatabaseManagerSwing\n\n"
         + "Copyright (c) 1995-2000, The Hypersonic SQL Group.\n"
         + "Copyright (c) 2001-2005, The HSQL Development Group.\n"
         + "http://hsqldb.org\n\n\n"
@@ -1366,7 +1366,7 @@ implements ActionListener, WindowListener, KeyListener {
                 if (sCmd.startsWith("-->>>TEST<<<--")) {
                     testPerformance();
                 } else {
-                    executeSQL();
+                    executeSQL(sCmd);
                 }
 
                 updateResult();
@@ -1385,10 +1385,9 @@ implements ActionListener, WindowListener, KeyListener {
     }
     ;
 
-    private void executeSQL() {
+    private void executeSQL(String sql) {
 
         String[] g   = new String[1];
-        String   sql = txtCommand.getText();
 
         try {
             lTime = System.currentTimeMillis();
