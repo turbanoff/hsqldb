bbb4e853b6d2c94ea0ee8aad0a997cd758558744
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2009-02-02 23:11:17 +0000

    Exact copy of trunk at current revision. The src directory contains the pristine starting point of code from the psqlodbc project, exported at 2009-02-02 UTC.

---
+++ odbc/tags/psqlodbc-pristine/src/version.h
@@ -0,0 +1,17 @@
+/* File:			version.h
+ *
+ * Description:		This file defines the driver version.
+ *
+ * Comments:		See "notice.txt" for copyright and license information.
+ *
+ */
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define POSTGRESDRIVERVERSION		"08.03.0401"
+#define POSTGRES_RESOURCE_VERSION	"08.03.0401\0"
+#define PG_DRVFILE_VERSION		8,3,04,01
+#define PG_BUILD_VERSION		"200901180001"
+
+#endif
