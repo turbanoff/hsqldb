15f98f3b8b0e450e491132cc911bbc802a08fe28
boucherb <boucherb@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2009-02-10 06:40:59 +0000

    merge of old 1.8.0.7 patches against 1.8.0.10 release

---
+++ dotnet/trunk/System.Data.Hsqldb/Lib/Etc/restest/resdb/src/db/dummy.java
@@ -0,0 +1,21 @@
+package db;
+/*
+ * dummy.java
+ *
+ * Created on April 17, 2007, 2:24 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author boucherb@users
+ */
+public class dummy {
+    
+    /** Creates a new instance of dummy */
+    public dummy() {
+    }
+    
+}
