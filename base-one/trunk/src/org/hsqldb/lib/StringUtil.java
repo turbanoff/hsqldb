a0f9b1dbdc49899fb838c61ec004e4d26b8492cb
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2009-09-07 14:21:51 +0000

    code updates leading to 1.8.1 release

--- base-one/trunk/src/org/hsqldb/lib/StringUtil.java
+++ base-one/trunk/src/org/hsqldb/lib/StringUtil.java
@@ -44,6 +44,32 @@ import java.lang.reflect.Array;
  */
 public class StringUtil {
 
+    public static String toPaddedString(String source, int length, char pad,
+                                        boolean trailing) {
+
+        int len = source.length();
+
+        if (len >= length) {
+            return source;
+        }
+
+        StringBuffer sb = new StringBuffer(length);
+
+        if (trailing) {
+            sb.append(source);
+        }
+
+        for (int i = len; i < length; i++) {
+            sb.append(pad);
+        }
+
+        if (!trailing) {
+            sb.append(source);
+        }
+
+        return sb.toString();
+    }
+
     /**
      * Returns a string with non alphanumeric chars converted to the
      * substitute character. A digit first character is also converted.
